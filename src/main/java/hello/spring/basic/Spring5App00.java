2c8caf8bbbcbb49c602216860e325c0fc264a113
jupiter002 <jik7194@naver.com>
2023-06-23 11:34:55 +0900

    sayHello메서드 매개변수 추가

--- src/main/java/hello/spring/basic/Spring5App00.java
+++ src/main/java/hello/spring/basic/Spring5App00.java
@@ -1,14 +1,17 @@
 package hello.spring.basic;
 
 public class Spring5App00 {
+    private static void sayHello(String name){
+        System.out.println(name+"입니다 Hello,World");
+    }
     public static void main(String[] args){
         //첫번째 사례
         //간단한 인삿말을 출력하는 프로그램 작성
-        sayHello();
+        sayHello("jupiter");
+
+
     }
-    private static void sayHello(){
-        System.out.println("Hello,World");
-    }
+
 
 
 }
