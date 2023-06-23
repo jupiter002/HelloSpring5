ee84b05310bdc51ffee82f5945ccefabd31dc679
jupiter002 <jik7194@naver.com>
2023-06-23 11:56:13 +0900

    개선사항 추가

--- src/main/java/hello/spring/bean/Spring5BEan01.java
+++ src/main/java/hello/spring/bean/Spring5BEan01.java
@@ -1,6 +1,14 @@
 package hello.spring.bean;
 
 public class Spring5BEan01 {
+    // 여기서 보듯, Spring5App01을 실행하려면
+    // 반드시 Spring5Bean01이라는 클래스가 필수임
+    // 즉, 클래스간의 의존성이 커짐
+
+    // 의존성이 커짐으로 인해 발생하는 부수적인 단점은
+    // 코드변경시 그것과 연관되는 다른 클래스에 영향을 미침
+    // 따라서, 유지보수의 범위가 넓어짐
+    // 해결 : 인터페이스를 도입해서 객체간의 의존성을 낮춰줌
     public void sayHello(String name){
         System.out.println(name+"입니다 Hello,World");
     }
