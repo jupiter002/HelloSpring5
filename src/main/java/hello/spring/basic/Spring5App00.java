15702f7700c5bd7c4c6a4aea0205458019e05cd1
jupiter002 <jik7194@naver.com>
2023-06-23 11:00:35 +0900

    spring_tutorial

---
+++ src/main/java/hello/spring/basic/Spring5App00.java
@@ -0,0 +1,10 @@
+package hello.spring.basic;
+
+public class Spring5App00 {
+    public static void main(String[] args){
+        //첫번째 사례
+        //간단한 인삿말을 출력하는 프로그램 작성
+        System.out.println("Hello,World");
+    }
+
+}
