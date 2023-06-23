943a48f949a1a8e8a1dbe82b6b79bf1bceb7f774
jupiter002 <jik7194@naver.com>
2023-06-23 13:34:17 +0900

    인삿말기능을 인터페이스 기반으로 작성

---
+++ src/main/java/hello/spring/bean/Spring5Bean03Eng.java
@@ -0,0 +1,11 @@
+package hello.spring.bean;
+
+public class Spring5Bean03Eng implements Spring5Bean03 {
+    //세번째 사례
+    //인삿말을 한국어,영어,일본어로 출력하도록 기능 추가
+    public void sayHello(String msg){
+        System.out.println("Hello "+msg);
+    }
+
+
+}
